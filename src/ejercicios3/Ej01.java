3fc011013bc74bc9ebc0262c839df5ec42e32851
Alejandro <Alejandro@DESKTOP-MT1TGFL>
2023-06-19 12:29:59 +0200

    Commit inicial

---
+++ src/ejercicios3/Ej01.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicios3;
+
+/**
+ * @author Alejandro
+ *
+ */
+public class Ej01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
